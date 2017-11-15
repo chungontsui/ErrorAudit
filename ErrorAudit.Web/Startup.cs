cd13160313adf04856d5f059ede9ddf4f596d0bf
chungontsui <chungont@gmail.com>
2017-11-15 23:51:45 +1300

    Added Web Project

---
+++ ErrorAudit.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ErrorAudit.Web.Startup))]
+namespace ErrorAudit.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
