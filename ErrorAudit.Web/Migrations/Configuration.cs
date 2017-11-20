81eb7d2129b0089a17ebe71f8896e6147a9564a3
chungontsui <chungont@gmail.com>
2017-11-20 23:47:31 +1300

    Added OrganizationId to the Identity Model but need to modify it so can add Organization Name

---
+++ ErrorAudit.Web/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace ErrorAudit.Web.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ErrorAudit.Web.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "ErrorAudit.Web.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(ErrorAudit.Web.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
