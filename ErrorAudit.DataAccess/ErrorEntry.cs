91a1ef8869b070c3a728a54cb94fcc505a08be83
chungontsui <chungont@gmail.com>
2017-11-23 00:14:03 +1300

    Progress, need to see if Azure has any reporting services

--- ErrorAudit.DataAccess/ErrorEntry.cs
+++ ErrorAudit.DataAccess/ErrorEntry.cs
@@ -16,11 +16,16 @@ namespace ErrorAudit.DataAccess
     {
         public int Id { get; set; }
         public int OrganizationId { get; set; }
-        public int StaffId { get; set; }
         public int ErrorId { get; set; }
         public System.DateTime CreatedDate { get; set; }
         public Nullable<System.DateTime> UpdateDate { get; set; }
         public Nullable<int> CompletedStaffId { get; set; }
         public Nullable<int> OutcomeId { get; set; }
+        public int ProcessingStaffEnter { get; set; }
+        public int ProcessingStaffDispensing { get; set; }
+        public int ProcessingStaffChecked { get; set; }
+        public int NoticedStaffEnter { get; set; }
+        public int NoticedStaffDispensing { get; set; }
+        public int NoticedStaffChecked { get; set; }
     }
 }
