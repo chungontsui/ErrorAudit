8a3ca10aed0302f28384b5934f6b879e04027840
chungontsui <chungont@gmail.com>
2017-12-11 23:28:04 +1300

    Progress

--- ErrorAudit.Web/Controllers/ReportController.cs
+++ ErrorAudit.Web/Controllers/ReportController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ErrorAudit.DataAccess;
+using ErrorAudit.Context.Entities;
+
 
 namespace ErrorAudit.Web.Controllers
 {
     public class ReportController : Controller
     {
+		private ConfigDataAccess DA;
+
+		public ReportController() {
+			DA = new ConfigDataAccess();
+		}
         // GET: Report
         public ActionResult Index()
         {
+			ViewBag.AllErrors = DA.GetError();
+
             return View();
         }
     }
