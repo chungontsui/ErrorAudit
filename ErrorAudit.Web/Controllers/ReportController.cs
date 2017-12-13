39ebe981373a172cf857a588fc4c2c6bb67af784
chungontsui <chungont@gmail.com>
2017-12-13 17:08:38 +1300

    Progress not much...

--- ErrorAudit.Web/Controllers/ReportController.cs
+++ ErrorAudit.Web/Controllers/ReportController.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using ErrorAudit.DataAccess;
+using ErrorAudit.DataAccess.ViewModel;
 using ErrorAudit.Context.Entities;
+
 
 
 namespace ErrorAudit.Web.Controllers
@@ -19,24 +21,33 @@ namespace ErrorAudit.Web.Controllers
         // GET: Report
         public ActionResult Index()
         {
-			List<Error> sampleErrors = new List<Error>();
-			sampleErrors.Add(new Error() { ErrorCode = "", ErrorDescription = "History not checked" });
-			sampleErrors.Add(new Error() { ErrorCode = "", ErrorDescription = "Dose innappropriate" });
-			sampleErrors.Add(new Error() { ErrorCode = "", ErrorDescription = "Incorrect Brand Choosen" });
-			sampleErrors.Add(new Error() { ErrorCode = "", ErrorDescription = "Incorrect Quantity/Period Of Supply" });
-			sampleErrors.Add(new Error() { ErrorCode = "", ErrorDescription = "Incorreect Calculations" });
-			sampleErrors.Add(new Error() { ErrorCode = "", ErrorDescription = "Incorrect Medicine" });
-			sampleErrors.Add(new Error() { ErrorCode = "", ErrorDescription = "Incorrect Strength" });
-			sampleErrors.Add(new Error() { ErrorCode = "", ErrorDescription = "Incorrect Quantity" });
-			sampleErrors.Add(new Error() { ErrorCode = "", ErrorDescription = "Correct Product Form" });
-			ViewBag.AllErrors = sampleErrors;//DA.GetError();
+			List<Error> sampleErrorHeadings = new List<Error>();
+			sampleErrorHeadings.Add(new Error() { ErrorCode = "", ErrorDescription = "History not checked" });
+			sampleErrorHeadings.Add(new Error() { ErrorCode = "", ErrorDescription = "Dose innappropriate" });
+			sampleErrorHeadings.Add(new Error() { ErrorCode = "", ErrorDescription = "Incorrect Brand Choosen" });
+			sampleErrorHeadings.Add(new Error() { ErrorCode = "", ErrorDescription = "Incorrect Quantity/Period Of Supply" });
+			sampleErrorHeadings.Add(new Error() { ErrorCode = "", ErrorDescription = "Incorreect Calculations" });
+			sampleErrorHeadings.Add(new Error() { ErrorCode = "", ErrorDescription = "Incorrect Medicine" });
+			sampleErrorHeadings.Add(new Error() { ErrorCode = "", ErrorDescription = "Incorrect Strength" });
+			sampleErrorHeadings.Add(new Error() { ErrorCode = "", ErrorDescription = "Incorrect Quantity" });
+			sampleErrorHeadings.Add(new Error() { ErrorCode = "", ErrorDescription = "Correct Product Form" });
+			ViewBag.AllErrors = sampleErrorHeadings;//DA.GetError();
 
-			var errorEntries = DA.GetErrorEntryViewModel();
+			//var errorEntries = DA.GetErrorEntryReportViewModel();
 
+			List<ErrorEntryReportViewModel> errorEntries = new List<ErrorEntryReportViewModel>();
 
+			//errorEntries = DA.GetErrorEntryReportViewModel();
+
+			errorEntries.Add(new ErrorEntryReportViewModel() {  })
 
             return View();
         }
+
+		private List<bool> GenerateRandomBoolList(int range)
+		{
+
+		}
     }
 
 
